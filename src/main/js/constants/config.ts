df7a7f738c18b82d49c7e2519111f48298da680b
Boik <qazbnm456@gmail.com>
2018-01-11 01:09:36 +0800

    mea culpa

--- src/main/js/constants/config.ts
+++ src/main/js/constants/config.ts
@@ -15,9 +15,5 @@ export default {
   lulumiPreloadPath: `${lulumiHelperPath}/preload`,
   lulumiPagesPath: `${lulumiHelperPath}/pages`,
   lulumiPDFJSPath: `${lulumiHelperPath}/pdfjs`,
-<<<<<<< HEAD
   lulumiRev: '75a49916e8f548f9c0db7eca291c8a8786613943',
-=======
-  lulumiRev: '5cd3a12342d810d409afe2a9fa401e0e2ebda2f9',
->>>>>>> accfe63... typos fixed & update dependencies
 };
