4d2426be4e6c40701ad022b498a24c4526cc4719
Boik <qazbnm456@gmail.com>
2018-01-23 15:16:49 +0800

    add support of about:gpu

--- src/renderer/js/lib/url-resource.ts
+++ src/renderer/js/lib/url-resource.ts
@@ -9,20 +9,22 @@ const urlResource = {
     switch (url) {
       case 'about:about':
         return getLulumiExtUrl('about/#/');
-      case 'about:lulumi':
-        return getLulumiExtUrl('about/#/lulumi');
-      case 'about:preferences':
-        return getLulumiExtUrl('about/#/preferences');
-      case 'about:downloads':
-        return getLulumiExtUrl('about/#/downloads');
-      case 'about:history':
-        return getLulumiExtUrl('about/#/history');
-      case 'about:extensions':
-        return getLulumiExtUrl('about/#/extensions');
-      case 'about:newtab':
-        return getLulumiExtUrl('about/#/newtab');
       case 'about:blank':
         return url;
+      case 'about:downloads':
+        return getLulumiExtUrl('about/#/downloads');
+      case 'about:extensions':
+        return getLulumiExtUrl('about/#/extensions');
+      case 'about:history':
+        return getLulumiExtUrl('about/#/history');
+      case 'about:lulumi':
+        return getLulumiExtUrl('about/#/lulumi');
+      case 'about:newtab':
+        return getLulumiExtUrl('about/#/newtab');
+      case 'about:preferences':
+        return getLulumiExtUrl('about/#/preferences');
+      case 'about:gpu':
+        return 'chrome://gpu';
       default:
         return getLulumiExtUrl('about/#/');
     }
