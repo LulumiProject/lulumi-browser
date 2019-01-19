95d2ac9489b1b39bf297805bf1277cd7180b7a8d
Boik <qazbnm456@gmail.com>
2019-01-19 15:47:27 +0800

    chore: update Electron to v3.1.1 & dependencies

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -14,5 +14,5 @@ export default {
   lulumiPreloadPath: `${path.resolve(lulumiRootPath, 'dist')}`,
   lulumiPagesPath: `${path.resolve(lulumiHelperPath, 'pages')}`,
   lulumiPDFJSPath: `${path.resolve(lulumiHelperPath, 'pdfjs')}`,
-  lulumiRev: 'e0d238adcdbde37e9000f3f5e28a64c85591a3b0',
+  lulumiRev: '25939aec84a268908f00e16a1b77754d8aa6f4f6',
 };
