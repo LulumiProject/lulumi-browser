bee2c9cb4c41e397b4eab9a463512b4920824e58
Boik <qazbnm456@gmail.com>
2019-02-05 14:14:08 +0800

    chore: update dependencies

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -14,5 +14,5 @@ export default {
   lulumiPreloadPath: `${path.resolve(lulumiRootPath, 'dist')}`,
   lulumiPagesPath: `${path.resolve(lulumiHelperPath, 'pages')}`,
   lulumiPDFJSPath: `${path.resolve(lulumiHelperPath, 'pdfjs')}`,
-  lulumiRev: '4b5944e82481afe6f8b2294a63fb9b446605d64a',
+  lulumiRev: '532c890c76083406442c71b6a1b52669745ed964',
 };
